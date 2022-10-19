9fede2776af122cac984d6388545e48167c9446a
tahaaltuntasdev <m.tahaaltuntas@gmail.com>
2022-10-19 20:44:42 +0300

    move package

--- app/src/main/java/com/tahaaltuntas/nasapp/view/Opportunity.kt
+++ app/src/main/java/com/tahaaltuntas/nasapp/view/Opportunity.kt
@@ -1,10 +1,11 @@
-package com.tahaaltuntas.nasapp
+package com.tahaaltuntas.nasapp.view
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.tahaaltuntas.nasapp.R
 
 
 class Opportunity : Fragment() {
